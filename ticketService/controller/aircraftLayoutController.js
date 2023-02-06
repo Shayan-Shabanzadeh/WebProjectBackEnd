ecd387501de54d24ef97d7440b4d7b60cd9ec4f6
Shayan <shayanshabanzadeh0@gmail.com>
2023-02-07 00:59:24 +0330

    refactor: add verifyJwt to aircraftLayoutController

--- ticketService/controller/aircraftLayoutController.js
+++ ticketService/controller/aircraftLayoutController.js
@@ -1,19 +1,22 @@
-const express = require('express');
-const AirCraftService = require('../service/airCraftLayoutService');
+const express = require("express");
+const AirCraftService = require("../service/airCraftLayoutService");
 const AircraftLayoutRouter = express.Router();
+const { verifyJwt } = require("../utils/utils");
 
-AircraftLayoutRouter.get('/:aircraftid',async (req, res, next)=>{
-	try {
-		const result = await AirCraftService.getAirCraftLayoutByLayoutId(
-			req.params.aircraftid
-		);
-		res.status(200).json({
-			msg: 'success',
-			data: result
-		})
-	}catch (e) {
-		next(e)
-	}
-})
+AircraftLayoutRouter.get("/:aircraftid", async (req, res, next) => {
+  
+  var result =  await verifyJwt();
+  try {
+    const result = await AirCraftService.getAirCraftLayoutByLayoutId(
+      req.params.aircraftid
+    );
+    res.status(200).json({
+      msg: "success",
+      data: result,
+    });
+  } catch (e) {
+    next(e);
+  }
+});
 
 module.exports = AircraftLayoutRouter;
