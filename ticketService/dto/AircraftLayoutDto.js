6aba6c07a04de65f1cb48502b6c44303ec47d41a
Shayan <shayanshabanzadeh0@gmail.com>
2023-02-06 19:01:06 +0330

    refactor:change AircraftLayout to AircraftLayoutDto, add type_id to AircraftLayoutDto ,

--- ticketService/dto/AircraftLayoutDto.js
+++ ticketService/dto/AircraftLayoutDto.js
@@ -3,12 +3,14 @@ class AircraftLayout{
 	y_class_capacity;
 	j_class_capacity;
 	f_class_capacity;
+	type_id;
 
 	constructor(data) {
 		this.layout_id = data.layout_id;
 		this.y_class_capacity = data.y_class_capacity;
 		this.f_class_capacity = data.f_class_capacity;
 		this.j_class_capacity = data.j_class_capacity
+		this.type_id = data.type_id;
 	}
 }
 
