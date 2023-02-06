771359d7b2d36f40cc04fb4eb110fecba9cae00a
Hesam Asnaashari <hessamasna@yahoo.com>
2023-02-06 14:36:32 +0330

    Airport getAirportByIataCode & getAirportByCountry & getAllAirports Done

--- ticketService/controller/AirportController.js
+++ ticketService/controller/AirportController.js
@@ -3,11 +3,35 @@ const AirportService = require("./../service/AirportService");
 
 const AirportRouter = express.Router();
 
-AirportRouter.get("/:iata_code", async (req, res, next) => {
+AirportRouter.get("/iata_code=:iata_code", async (req, res, next) => {
     try {
         const result = await AirportService.getAirportByIataCode(
             req.params.iata_code
         );
+        res.status(200).json({
+            msg: "success",
+            data: result,
+        });
+    } catch (err) {
+        next(err);
+    }
+});
+AirportRouter.get("/country_name=:country_name", async (req, res, next) => {
+    try {
+        const result = await AirportService.getAirportByCountry(
+            req.params.country_name
+        );
+        res.status(200).json({
+            msg: "success",
+            data: result,
+        });
+    } catch (err) {
+        next(err);
+    }
+});
+AirportRouter.get("/", async (req, res, next) => {
+    try {
+        const result = await AirportService.getAllAirports();
         res.status(200).json({
             msg: "success",
             data: result,
--- ticketService/service/AirportService.js
+++ ticketService/service/AirportService.js
@@ -1,22 +1,54 @@
 const AirportRepository = require('../repository/AirportRepository');
 
 class AirportService {
-	getAirportByIataCode = async (iata_code) =>{
-		try {
-			const result = await AirportRepository.findAirportById(iata_code);
-			if (!result){
-				const error = new Error(
-					`couldnt find aircraft Airport by iata_code: ${iata_code}`
-				);
-				error.status = 404
-				throw error
-			}else{
-				return result;
-			}
-		}catch (e) {
-			throw e
-		}
-	}
+    getAirportByIataCode = async (iata_code) => {
+        try {
+            const result = await AirportRepository.findAirportById(iata_code);
+            if (!result) {
+                const error = new Error(
+                    `couldnt find aircraft Airport by iata_code: ${iata_code}`
+                );
+                error.status = 404
+                throw error
+            } else {
+                return result;
+            }
+        } catch (e) {
+            throw e
+        }
+    }
+    getAirportByCountry = async (country_name) => {
+        try {
+            const result = await AirportRepository.getAirportByCountry(country_name);
+            if (!result) {
+                const error = new Error(
+                    `couldnt find aircraft Airport by country_name: ${country_name}`
+                );
+                error.status = 404
+                throw error
+            } else {
+                return result;
+            }
+        } catch (e) {
+            throw e
+        }
+    }
+    getAllAirports = async () => {
+        try {
+            const result = await AirportRepository.getAllAirports();
+            if (!result) {
+                const error = new Error(
+                    `couldnt find aircraft Airport`
+                );
+                error.status = 404
+                throw error
+            } else {
+                return result;
+            }
+        } catch (e) {
+            throw e
+        }
+    }
 }
 
 module.exports = new AirportService();
