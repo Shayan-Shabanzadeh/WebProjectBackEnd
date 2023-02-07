c70b09d642114e05afbb0458a6674731d705aa00
Shayan <shayanshabanzadeh0@gmail.com>
2023-02-07 14:38:55 +0330

    refactor: add some example for jwtVerfication, getTransaction and createTransaction in aircraftLayoutController.js

--- ticketService/controller/aircraftLayoutController.js
+++ ticketService/controller/aircraftLayoutController.js
@@ -1,17 +1,51 @@
 const express = require("express");
 const AirCraftService = require("../service/airCraftLayoutService");
 const AircraftLayoutRouter = express.Router();
-const { verifyJwtFromCookie } = require("../utils/utils");
+const {
+  verifyJwtFromCookie,
+  getTransactionWithId,
+  createTransaction,
+} = require("../utils/utils");
 
 AircraftLayoutRouter.get("/:aircraftid", async (req, res, next) => {
   try {
     //jwt verification
+
     const isValid = await verifyJwtFromCookie(req);
     if (isValid === false) {
       const err = new Error("invalid token");
       err.status = 403;
       throw err;
     }
+
+    //create bank trasnaction 
+
+    const r = await createTransaction({
+      amount: 10000,
+      receipt_id: 1,
+      callback: "localhost:3000",
+    });
+
+    if (r.statusCode === 201) {
+      console.log("transaction created");
+      console.log(r);
+    } else {
+      const err = new Error("Failed to create transaction");
+      err.status = r.statusCode;
+      throw err;
+    }
+
+    // get a transaction from bank 
+    const response = await getTransactionWithId(1);
+    if (response.statusCode === 200) {
+      console.log("transaction found")
+      console.log(response.body);
+    }else{
+      const err = new Error("Failed to get transaction ");
+      err.status = r.statusCode;
+      throw err;
+    }
+
     //
     const result = await AirCraftService.getAirCraftLayoutByLayoutId(
       req.params.aircraftid
