ccff97b999caef130520da3659e9f0019615ab0d
Shayan <shayanshabanzadeh0@gmail.com>
2023-02-07 14:37:30 +0330

    add: connection to bank service , add: getTransactionWithId , add: createTransaction

--- ticketService/utils/utils.js
+++ ticketService/utils/utils.js
@@ -1,4 +1,42 @@
 const client = require("../grpc/grpcInitializer");
+const request = require("request");
+
+const BANK_URL = process.env.BANK_URL || "http://127.0.0.1:8000/";
+
+const callGetTransactionFromBankApi = async (id) => {
+  return new Promise((resolve, reject) => {
+    request(BANK_URL + id, { json: true }, (err, res) => {
+      if (err) reject(err);
+      else resolve(res);
+    });
+  });
+};
+
+const callAddTransactionFromBankApi = async (FormData) => {
+  return new Promise((resolve, reject) => {
+    request.post(
+      {
+        url: BANK_URL,
+        body: FormData,
+        json: true,
+      },
+      function (error, response) {
+        if (error) reject(error);
+        else resolve(response);
+      }
+    );
+  });
+};
+
+const createTransaction = async (FormData) => {
+  const result = await callAddTransactionFromBankApi(FormData);
+  return { body: result.body, statusCode: result.statusCode };
+};
+
+const getTransactionWithId = async (id) => {
+  const result = await callGetTransactionFromBankApi(id);
+  return { body: result.body, statusCode: result.statusCode };
+};
 
 const verifyJwtFromCookie = async (req) => {
   try {
@@ -55,4 +93,10 @@ function getAccessToken(req) {
   return acces_token;
 }
 
-module.exports = { verifyJwt, getAccessToken, verifyJwtFromCookie };
+module.exports = {
+  verifyJwt,
+  getAccessToken,
+  verifyJwtFromCookie,
+  getTransactionWithId,
+  createTransaction,
+};
