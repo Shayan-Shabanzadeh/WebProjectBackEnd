c88210ed841b56f0ec901085f287522ea6849301
Shayan <shayanshabanzadeh0@gmail.com>
2023-02-07 20:02:24 +0330

    refactor: change the examples for jwt validation , getTransaction , createTransaction

--- ticketService/controller/aircraftLayoutController.js
+++ ticketService/controller/aircraftLayoutController.js
@@ -1,50 +1,30 @@
 const express = require("express");
 const AirCraftService = require("../service/airCraftLayoutService");
 const AircraftLayoutRouter = express.Router();
-const {
-  verifyJwtFromCookie,
-  getTransactionWithId,
-  createTransaction,
-} = require("../utils/utils");
+const Utils = require("../utils/utils");
 
 AircraftLayoutRouter.get("/:type_id", async (req, res, next) => {
   try {
     //jwt verification
 
-    const isValid = await verifyJwtFromCookie(req);
-    if (isValid === false) {
-      const err = new Error("invalid token");
-      err.status = 403;
-      throw err;
-    }
+    const isValid = await Utils.verifyJwtFromCookie(req);
+    // if (isValid === false) {
+    //   const err = new Error("invalid token");
+    //   err.status = 403;
+    //   throw err;
+    // }
 
-    //create bank trasnaction 
+    //create bank trasnaction
 
-    const r = await createTransaction({
-      amount: 10000,
-      receipt_id: 1,
-      callback: "localhost:3000",
-    });
+    // const r = await Utils.createTransaction({
+    //   amount: 10000,
+    //   receipt_id: 1,
+    //   callback: "localhost:3000",
+    // });
 
-    if (r.statusCode === 201) {
-      console.log("transaction created");
-      console.log(r);
-    } else {
-      const err = new Error("Failed to create transaction");
-      err.status = r.statusCode;
-      throw err;
-    }
-
-    // get a transaction from bank 
-    const response = await getTransactionWithId(1);
-    if (response.statusCode === 200) {
-      console.log("transaction found")
-      console.log(response.body);
-    }else{
-      const err = new Error("Failed to get transaction ");
-      err.status = r.statusCode;
-      throw err;
-    }
+    // get a transaction from bank
+    const response = await Utils.getTransactionWithId(1);
+    console.log(response);
 
     //
     const result = await AirCraftService.getAirCraftLayoutByLayoutId(
